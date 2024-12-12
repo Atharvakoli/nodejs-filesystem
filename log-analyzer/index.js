f40077b05161f9aec802734f3d2b0b545d3ec249
Atharvakoli <koliatharva0@gmail.com>
2024-12-12 10:38:49 +0530

    log-analyzer updated

---
+++ log-analyzer/index.js
@@ -0,0 +1,87 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+
+const logFilePath = process.env.LOG_FILE_PATH;
+
+function summarizeLogs() {
+  try {
+    const data = fs.readFileSync(logFilePath, "utf-8");
+    const lines = data.trim().split("\n");
+    let errorCount = 0;
+    let warningCount = 0;
+    lines.forEach((line) => {
+      if (line.includes("ERROR")) {
+        errorCount++;
+      } else if (line.includes("WARN")) {
+        warningCount++;
+      }
+    });
+    console.log("Summary: ");
+    console.log("Total Errors: ", errorCount);
+    console.log("Total Warning: ", warningCount);
+  } catch (error) {
+    console.error("Error while summarizing: ", error);
+  }
+}
+
+function extractErrorMessages() {
+  try {
+    const data = fs.readFileSync(logFilePath, "utf-8");
+    const lines = data.trim().split("\n");
+
+    const errorMessages = lines.filter((line) => line.includes("ERROR"));
+    let erroLogFile = path.join(__dirname, "errorMessages.log");
+    fs.writeFileSync(erroLogFile, errorMessages.join("\n"), "utf-8");
+  } catch (error) {
+    console.error("Error while extracting: ", error.message);
+  }
+}
+
+function countLogLevel() {
+  try {
+    const data = fs.readFileSync(logFilePath, "utf-8");
+    const lines = data.trim().split("\n");
+    let info = 0,
+      warn = 0,
+      error = 0;
+    lines.forEach((line) => {
+      if (line.includes("ERROR")) error++;
+      if (line.includes("WARN")) warn++;
+      if (line.includes("INFO")) info++;
+    });
+    console.log(`INFO Count: ${info}\n
+WARN Count: ${warn}\n
+ERROR Count: ${error}`);
+  } catch (error) {
+    console.error("Error while file reading ", error);
+  }
+}
+// countLogLevel();
+
+function logFileAnalyzer() {
+  let toStop = true;
+  try {
+    while (toStop) {
+      console.log(
+        "Choose an option:\n 1. Summarize Errors and Warnings\n 2. Extract Error Messages 3. To stop"
+      );
+      let userInput = prompt("Enter your choice: ");
+      switch (userInput) {
+        case 1:
+          summarizeLogs();
+          break;
+        case 2:
+          extractErrorMessages();
+        case 3:
+          toStop = false;
+        default:
+          console.log("Wrong input");
+          break;
+      }
+    }
+  } catch (error) {
+    console.error("Error while file loging", error.message);
+  }
+}
+logFileAnalyzer();
